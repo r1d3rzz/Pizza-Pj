629f9b03ca06e7a25159b6a5227a65b6c6388582
r1d3rzz <myintthwaykhine127@gmail.com>
2022-10-06 11:01:36 +0630

    create pizza added ui

---
+++ resources/views/pizza/index.blade.php
@@ -0,0 +1,76 @@
+<x-layout>
+    <x-slot name="title">
+        Pizza House
+    </x-slot>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">Menu</div>
+
+                    <div class="card-body">
+                        <div class="form-group">
+                            <ul class="list-group">
+                                <li class="list-group-item list-group-item-action">View</li>
+                                <li class="list-group-item list-group-item-action">Add</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Pizza</div>
+
+                    <div class="card-body">
+                        <div class="form-group mb-3">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" id="name" placeholder="name of pizza">
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="description">Description</label>
+                            <textarea type="text" name="description" id="description" class="form-control"
+                                placeholder="name of pizza"></textarea>
+                        </div>
+
+                        <div class="form-group mb-3 d-flex align-items-center">
+                            <div class="me-3">
+                                <label>Pizza price($)</label>
+                            </div>
+                            <div class="mx-2">
+                                <input type="number" class="form-control" placeholder="small pizza price">
+                            </div>
+                            <div class="mx-2">
+                                <input type="number" class="form-control" placeholder="medium pizza price">
+                            </div>
+                            <div class="mx-2">
+                                <input type="number" class="form-control" placeholder="large pizza price">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="category">Category</label>
+                            <select class="form-control">
+                                <option value=""></option>
+                                <option value="vegetarian">Vegetarian</option>
+                                <option value="nonvegetarian">Non-vegetarian</option>
+                                <option value="traditional">Traditional</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="image">Image</label>
+                            <input type="file" name="image" id="image" class="form-control">
+                        </div>
+
+                        <div class="d-flex justify-content-end">
+                            <button class="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PizzaController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//for Pizza Routes
+Route::get('/pizza',[PizzaController::class,'index'])->name('pizza.index');
