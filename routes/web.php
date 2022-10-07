474ab64d3961999637c181c019c385a882fce500
r1d3rzz <myintthwaykhine127@gmail.com>
2022-10-07 10:58:49 +0630

    create admin middleware

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 //for Pizza Routes
-Route::get('/pizza',[PizzaController::class,'index'])->name('pizza.index');
-Route::get('/pizza/create',[PizzaController::class,'create'])->name('pizza.create');
-Route::post('/pizza/store',[PizzaController::class,'store'])->name('pizza.store');
-Route::get('/pizza/{id}/edit',[PizzaController::class,'edit'])->name('pizza.edit');
-Route::put('/pizza/{id}/update',[PizzaController::class,'update'])->name('pizza.update');
-Route::delete('/pizza/{id}/destroy',[PizzaController::class,'destroy'])->name('pizza.destroy');
+Route::middleware(['admin','auth'])->group(function(){
+    Route::get('/pizza',[PizzaController::class,'index'])->name('pizza.index');
+    Route::get('/pizza/create',[PizzaController::class,'create'])->name('pizza.create');
+    Route::post('/pizza/store',[PizzaController::class,'store'])->name('pizza.store');
+    Route::get('/pizza/{id}/edit',[PizzaController::class,'edit'])->name('pizza.edit');
+    Route::put('/pizza/{id}/update',[PizzaController::class,'update'])->name('pizza.update');
+    Route::delete('/pizza/{id}/destroy',[PizzaController::class,'destroy'])->name('pizza.destroy');
+});
